8397345adc1d2d42dc3356194a4ffc95b1c91120
dinextw <dinex.box@gmail.com>
2018-08-15 15:07:19 +0800

    Store travel time of grid points
    Add test cases

--- datastore.py
+++ datastore.py
@@ -72,8 +72,17 @@ class DataStore(object):
         self._cursor = self._db.cursor()
         self._norm = normgrid.NormGrid()
 
-    def import_time(self, filepath):
-        #timenode = []
+    def import_time(self, idx_weight, idx_sta):
+        """ Import vertexes's travel time
+        Args:
+            idx_weight: dictionary of vertexes's travel time
+            idx_sta: index of station location
+        Returns:
+        Raises:
+            Native exceptions.
+        """
+        """
+        timenode = []
         with open(filepath, newline='') as file:
             csvreader = csv.reader(file)
             next(csvreader)
@@ -83,15 +92,15 @@ class DataStore(object):
                 for elem in row:
                     row_float.append(float(elem))
                 path.append(row_float)
-                #timenode.append(TimeNode(self._norm.get_norm_index(row_float[0:3],2),row_float[3]))
+                timenode.append(TimeNode(self._norm.get_norm_index(row_float[0:3],2),row_float[3]))
+        """
         sql = ("CREATE TABLE IF NOT EXISTS table_%d (ID BIGINT, TRAVEL_TIME FLOAT, PRIMARY KEY(ID))"
-               % self._norm.get_norm_index(path[-1][0:3], 2))
+               % idx_sta)
         self._cursor.execute(sql)
-        for vertex in path:
+        for idx, time in idx_weight.items():
             sql = ("INSERT INTO table_%d (id, travel_time) VALUES(%d, %f) \
                    ON DUPLICATE KEY UPDATE travel_time = LEAST(travel_time, VALUES(travel_time))"
-                   % (self._norm.get_norm_index(path[-1][0:3], 2)
-                      , self._norm.get_norm_index(vertex[0:3], 2), vertex[3]))
+                   % (idx_sta, idx, time))
             try:
                 self._cursor.execute(sql)
                 self._db.commit()
@@ -99,6 +108,17 @@ class DataStore(object):
                 self._db.rollback()
 
     def get_time(self, loc_sta, loc_sou):
+        """ Get the travel time of specific source
+        Args:
+            loc_sta: location of station
+            loc_sou: location of source
+        Returns:
+            travel_time: travel time of specific source
+        Raises:
+            Native exceptions.
+        """
+        assert loc_sta != loc_sou, 'Error in same station and source location'
+
         idx_sta = self._norm.get_norm_index(loc_sta, 2)
         idx_sou = self._norm.get_norm_index(loc_sou, 2)
         sql = "SELECT travel_time FROM table_%d WHERE id = %d" % (idx_sta, idx_sou)
@@ -110,21 +130,73 @@ class DataStore(object):
             print("Error in looking for travel time")
 
     def close_db(self):
+        """ Close the connection session
+        Args:
+        Returns:
+        Raises:
+            Native exceptions.
+        """
         self._db.close()
 
 
-class GraphTest(unittest.TestCase):
-    """ Test with vertex and edge correctness in graph
+def _test_mod_read_weight_file(filepath):
+    norm = normgrid.NormGrid()
+    with open(filepath, newline='') as file:
+        csvreader = csv.reader(file)
+        next(csvreader)
+        path = {}
+        count = 0
+        for row in csvreader:
+            row_float = []
+            for elem in row:
+                row_float.append(float(elem))
+            if count == 0:
+                loc_sou = row_float[0:3]
+                count += 1
+            path[norm.get_norm_index(row_float[0:3], 2)] = row_float[3]
+        idx = norm.get_norm_index(row_float[0:3], 2)
+        loc_sta = row_float[0:3]
+    return (path, idx, loc_sou, loc_sta)
+
+
+class DataTest(unittest.TestCase):
+    """ Test with Database write and read action
     """
+    def test_mod_with_duplicate_table(self):
+        """ Test if duplicate table will cause error in reading travel time
+        """
+        test = DataStore()
+
+        info_result1 = _test_mod_read_weight_file('./tmp/result1.csv')
+        test.import_time(info_result1[0], info_result1[1])
+        time1 = test.get_time(info_result1[3], info_result1[2])
+
+        info_result2 = _test_mod_read_weight_file('./tmp/result1.csv')
+        test.import_time(info_result2[0], info_result2[1])
+        time2 = test.get_time(info_result2[3], info_result2[2])
+        test.close_db()
+        self.assertEqual(time1, time2)
+
+    def test_mod_with_two_tables(self):
+        """ Test if different tables can be created in database
+        """
+        test = DataStore()
+
+        info_result1 = _test_mod_read_weight_file('./tmp/result1.csv')
+        info_result2 = _test_mod_read_weight_file('./tmp/result2.csv')
+        test.import_time(info_result1[0], info_result1[1])
+        test.import_time(info_result2[0], info_result2[1])
+        time1 = test.get_time(info_result1[3], info_result1[2])
+        time2 = test.get_time(info_result2[3], info_result2[2])
+        test.close_db()
+
+        self.assertNotEqual(time1, time2)
+
 
 def main():
     """ unit test
     """
-    test = DataStore()
-    test.import_time('./tmp/result.csv')
-    print(test.get_time([120, 23, 0], [120.01, 23.01, 1]))
-    test.close_db()
-    # --> check for Stage 1
+    unittest.main()
 
 
 if __name__ == '__main__':
